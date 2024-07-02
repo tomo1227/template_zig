5007a5a2b356b97469a43ce8b211a0dd79702d6a
Tomoki Ota <tomo.soccer1227@gmail.com>
2024-07-02 22:17:27 +0900

    Initial commit

---
+++ .devcontainer/scripts/post_start.sh
@@ -0,0 +1,12 @@
+if [ -z "$1" ]; then
+  echo "Error: No directory provided."
+  exit 1
+fi
+
+#  ~/.gitconfigをローカルからコピーする
+echo "start to copy local ~/.gitconfig"
+git config --global --add safe.directory $1
+
+# ~/.gitconfigの設定追加
+git config pull.rebase false
+git config --global commit.template .commit_template
